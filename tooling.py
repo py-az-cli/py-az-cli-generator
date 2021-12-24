8807c803a7811fb335601c1179dee914411a0f75
bigdatamoore <9993122+bigdatamoore@users.noreply.github.com>
2021-12-24 17:26:39 -0500

    updating source for tooling

--- tooling.py
+++ tooling.py
@@ -2,7 +2,7 @@
 # --------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
-# https://github.com/microsoft/vscode-azurecli/blob/main/service/azservice/tooling2.py
+# Source: https://github.com/microsoft/vscode-azurecli/blob/main/service/azservice/tooling2.py
 # --------------------------------------------------------------------------------------------
 from __future__ import print_function
 
